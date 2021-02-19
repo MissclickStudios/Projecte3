1a006e33e5a8540256048d1c1e7127ec4dee8d7f
bottzo <45202057+bottzo@users.noreply.github.com>
2021-02-19 19:11:48 +0100

    added the wwise libraries to the project

--- NULL Engine/Source/M_Audio.cpp
+++ NULL Engine/Source/M_Audio.cpp
@@ -1,3 +1,10 @@
+#ifndef _DEBUG
+#define AK_OPTIMIZED
+#else
+#pragma comment(lib,"CommunicationCentral.lib")
+#pragma comment(lib,"ws2_32.lib")
+#endif
+
 #include "M_Audio.h"
 
 M_Audio::M_Audio(bool isActive) : Module("Audio", isActive)
