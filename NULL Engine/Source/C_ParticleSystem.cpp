802b42a905040542ade95c59b081ce4a2c98d125
albertec1 <45201952+albertec1@users.noreply.github.com>
2021-03-30 12:29:18 +0200

    First approach to particle system interface

--- NULL Engine/Source/C_ParticleSystem.cpp
+++ NULL Engine/Source/C_ParticleSystem.cpp
@@ -35,9 +35,20 @@ bool C_ParticleSystem::LoadState(ParsonNode& root)
 
 bool C_ParticleSystem::Update()
 {
-	for (unsigned int i = 0; i < emitterInstances.size(); ++i)
+
+	if (previewEnabled == true && Time::Game::GetDT() == 0)
 	{
-		emitterInstances[i]->Update(Time::Game::GetDT());
+		for (unsigned int i = 0; i < emitterInstances.size(); ++i)
+		{
+			emitterInstances[i]->Update(Time::Real::GetDT());
+		}
+	}
+	else
+	{
+		for (unsigned int i = 0; i < emitterInstances.size(); ++i)
+		{
+			emitterInstances[i]->Update(Time::Game::GetDT());
+		}
 	}
 
 	return true;
@@ -66,6 +77,25 @@ void C_ParticleSystem::AddDefaultEmitter()
 {
 	defaultEmitter = new Emitter();
 	defaultEmitter->SetAsDefault();
+}
+
+void C_ParticleSystem::EnginePreview(bool previewEnabled)
+{
+	if (this->previewEnabled == previewEnabled)	//if the state of the particle system is different than the requested, clear all and switch mode.
+	{
+		return;
+	}
+
+	ClearEmitters();
+	this->previewEnabled = previewEnabled;
+}
+
+void C_ParticleSystem::ClearEmitters()
+{
+	for(int i = 0; i < emitterInstances.size(); i++)
+	{
+		emitterInstances[i]->KillAll();
+	}
 }
 
 void C_ParticleSystem::Reset()
