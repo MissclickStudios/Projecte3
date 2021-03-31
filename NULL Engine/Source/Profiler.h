aebcf8798fff27c4664469432a9bb9cba80ea169
bottzo <45202057+bottzo@users.noreply.github.com>
2021-03-31 18:19:32 +0200

    removed profiler and memo manager on build mode

--- NULL Engine/Source/Profiler.h
+++ NULL Engine/Source/Profiler.h
@@ -1,12 +1,12 @@
 #ifndef __PROFILER_H__
 #define __PROFILER_H__
 
-#ifndef GAMEBUILD
+#ifndef NOPROFILER
 #include "Dependencies/Optik/include/optick.h"
 
 #else
 #define OPTICK_FRAME(FRAME_NAME, ...)
 #define OPTICK_CATEGORY(NAME, CATEGORY)
-#endif // !GAMEBUILD
+#endif // !NOPROFILER
 
 #endif // !__PROFILER_H__
