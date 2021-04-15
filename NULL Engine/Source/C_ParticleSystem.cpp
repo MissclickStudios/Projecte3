1297401a4b2194ca520f1806cfda4a0d19436cee
paupedra <paupe200@gmail.com>
2021-04-15 15:25:59 +0200

    Clean Up particle Sisyem Resource/component

--- NULL Engine/Source/C_ParticleSystem.cpp
+++ NULL Engine/Source/C_ParticleSystem.cpp
@@ -1,5 +1,6 @@
 #include "JSONParser.h"
 
+#include "Emitter.h"
 #include "Time.h"
 #include "GameObject.h"
 
@@ -83,8 +84,25 @@ bool C_ParticleSystem::Update()
 	return true;
 }
 
+bool C_ParticleSystem::CleanUp()
+{
+	App->resourceManager->FreeResource(resource->GetUID());
+	resource = nullptr;
+
+	//Clean Emitter Instances
+	for (auto emitter = emitterInstances.begin(); emitter != emitterInstances.end(); ++emitter)
+	{
+		delete (*emitter);
+	}
+
+	emitterInstances.end();
+
+	return false;
+}
+
 void C_ParticleSystem::SetParticleSystem(R_ParticleSystem* newParticleSystem)
 {
+	CleanUp();
 	resource = newParticleSystem;
 	RefreshEmitters();
 }
